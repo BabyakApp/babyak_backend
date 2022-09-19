91d2643687aa52d97b1a1e89b641bc3fca058b43
Sooah Han <s2sooas2@ewhain.net>
2022-09-19 17:44:49 +0900

    chore: initial setting

---
+++ src/main/java/Babyak/babyak_backend/BabyakBackendApplication.java
@@ -0,0 +1,13 @@
+package Babyak.babyak_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabyakBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabyakBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Babyak/babyak_backend/BabyakBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package Babyak.babyak_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabyakBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
