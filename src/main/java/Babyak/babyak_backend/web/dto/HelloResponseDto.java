872a88bba5bbfba2a0a935ac98cd576369badbba
Yujin Seol <66419086+rkdfc93@users.noreply.github.com>
2022-09-27 20:00:20 +0900

    ~jpa 적용

---
+++ src/main/java/Babyak/babyak_backend/web/dto/HelloResponseDto.java
@@ -0,0 +1,13 @@
+package Babyak.babyak_backend.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
+}
